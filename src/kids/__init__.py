95116ee3f31a9f1cdc604bd0ed59d57cc4a64d3d
Valentin Lab <valentin.lab@kalysto.org>
2017-11-16 14:05:45 +0800

    fix: fixed import time performance issue due to obsolete namespacing pattern. (fixes #9)

--- src/kids/__init__.py
+++ src/kids/__init__.py
@@ -1,6 +1,4 @@
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+## From https://docs.python.org/3/library/pkgutil.html
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
